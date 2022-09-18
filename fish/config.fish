a5434fe9b288664160d7b794fd804e762e9b0895
rixVe <piotr.litewka.2006@gmail.com>
2022-09-18 14:24:00 +0200

    small changes

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,8 @@ export SHELL="/bin/fish"
 export PF_COL1=3
 export PF_COL2=8
 export PF_COL3=7
-export PF_INFO="ascii title os kernel host wm de uptime pkgs memory editor shell palette"
+# export PF_INFO="ascii title os kernel host wm de uptime pkgs memory editor shell palette"
+export PF_INFO="title os kernel host wm de uptime pkgs memory editor shell palette"
 export PF_ALIGN=8
 export PF_SEP=""
 
@@ -18,4 +19,4 @@ pfetch
 
 function fish_mode_prompt
 end
-export TERM=xterm-256color
+export TERM="xterm-256color"
